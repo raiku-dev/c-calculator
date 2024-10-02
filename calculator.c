9131a1cbdf91d7d6afb5dfdb6ff28704e9d5d4d6
raiku <mikayil_gacek@icloud.com>
2024-10-02 18:23:54 +0200

    refactor

--- calculator.c
+++ calculator.c
@@ -1,35 +1,46 @@
 #include <stdio.h>
 
 int main() {
-  int a, b, c;
-  c = 0;
+    float a, b;
+    int c;
+    c = 0;
 
-  while (c > 4 || c < 1) {
-    printf("What would you like to do?\n");
-    printf("1: add\n");
-    printf("2: substract\n");
-    printf("3: divide\n");
-    printf("4: multiply\n");
-    printf("5: quit\n");
-    scanf("%d", &c);
-  }
+    while (c > 5 || c < 1) {
+        printf("What would you like to do?\n");
+        printf("1: add\n");
+        printf("2: substract\n");
+        printf("3: divide\n");
+        printf("4: multiply\n");
+        printf("5: quit\n");
+        scanf("%d", &c);
 
-  printf("Choose first and second number:\n");
-  scanf("%d %d", &a, &b);
+        if (c == 5) {
+            printf("Goodbye!\n");
+            return 0;
+        }
+    }
 
-  if (c == 5) {
+    printf("Choose first and second number:\n");
+    scanf("%f %f", &a, &b);
+
+    switch (c) {
+    case 1:
+        printf("%f + %f = %f\n", a, b, a + b);
+        break;
+    case 2:
+        printf("%f - %f = %f\n", a, b, a - b);
+        break;
+    case 3:
+        while (b == 0) {
+            printf("You can't divide by zero!\n");
+            scanf("%f", &b);
+        }
+        printf("%f / %f = %f\n", a, b, a / b);
+        break;
+    case 4:
+        printf("%f * %f = %f\n", a, b, a * b);
+        break;
+    }
+
     return 0;
-  }
-
-  if (c == 1) {
-    printf("%d + %d = %d\n", a, b, a + b);
-  } else if (c == 2) {
-    printf("%d - %d = %d\n", a, b, a - b);
-  } else if (c == 3) {
-    printf("%d / %d = %d\n", a, b, a / b);
-  } else if (c == 4) {
-    printf("%d * %d = %d\n", a, b, a * b);
-  }
-
-  return 0;
 }
